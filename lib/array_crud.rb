9319be144a86e0efa976dd25706806c1b8505f68
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 00:49:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   new_array = [array, 5]
-  new_array.unshift(element)
+  new_array.unshift([-1])
 end
 
 def remove_element_from_end_of_array(array)
