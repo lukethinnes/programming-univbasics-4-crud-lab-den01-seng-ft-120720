2bb8eb95587838e389acbe91515e8a59e2bc3366
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 00:46:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  big_array = [array, element]
-  big_array << "Jason"
+  big_array = [array]
+  big_array << element
 end
 
 def add_element_to_start_of_array(array, element)
