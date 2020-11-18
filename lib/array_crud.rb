32d705a390bb8435463ec81d6ae4f78e37ab882a
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 00:51:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  this_array = [array, 2]
+  this_array = [array]
   this_array.pop([-1])
 end
 
