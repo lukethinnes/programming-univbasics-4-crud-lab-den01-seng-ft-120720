eb96721f1f2a4ea1b5e8156c87ba2848e4170b0e
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 00:55:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = [0,1,2,3,4,5,6,7,8,"arrays!"]
-  array.pop
+  array.pop()
 end
 
 def retrieve_element_from_index(array, index_number)
