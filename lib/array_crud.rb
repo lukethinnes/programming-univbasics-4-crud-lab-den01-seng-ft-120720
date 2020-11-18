d2e1db5ac75915165ac24daf8bd83f9a3dec5d07
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 00:57:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,13 +17,13 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = [0,1,2,3,4,5,6,7,8,"arrays!"]
+  array = [8, "field of dreams"]
   array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["wow", 9,8,7,6,5,4,3,2,1,0]
-array.shift
+  array = ["Joint Success", 67]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
