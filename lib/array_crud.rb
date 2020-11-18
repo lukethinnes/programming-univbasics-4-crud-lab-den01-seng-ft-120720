c022c38587f13b131c68866aef1aa560d85f8097
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 00:44:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,44 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["I", "Just", "Made" "Array"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  big_array = [array, element]
+  big_array << "Jason",
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  new_array = [array, element]
+  new_array.unshift("Doghouse")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  this_array = [array]
+  this_array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  high_array = [array]
+  high_array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  low_array [array, index_number]
+  return low_array
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array_array [array]
+  puts array_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  move_array[array, "Snow Dream"]
+  move_array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
